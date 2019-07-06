01277b871d4df5c5a52f355f8ae7822ea665f4d4
Stefanos Anagnostou <anagstef@gmail.com>
2019-07-06 14:44:59 +0300

    Add new toogle and smooth transition

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,39 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import './header.css';
 import IonicLogo from '../images/ionic-icon.png';
+import Toggle from './toggle';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggleDarkTheme = this.toggleDarkTheme.bind(this);
-  }
-
-  toggleDarkTheme() {
-    const { toggleDarkTheme, darkTheme } = this.props;
-    toggleDarkTheme(!darkTheme);
-  }
-
-  render() {
-    const { siteTitle, siteSubtitle } = this.props;
-    return (
-      <header>
-        <div className="header-container">
-          <div className="header-title-logo">
-            <img src={IonicLogo} className="ionic-logo" alt="ionic logo" />
-            <div className="header-title-text">
-              <h1>{siteTitle}</h1>
-              <h5>{siteSubtitle}</h5>
-            </div>
-          </div>
-          <button type="button" onClick={this.toggleDarkTheme}>?</button>
+const Header = ({ siteTitle, siteSubtitle }) => (
+  <header>
+    <div className="header-container">
+      <div className="header-title-logo">
+        <img src={IonicLogo} className="ionic-logo" alt="ionic logo" />
+        <div className="header-title-text">
+          <h1>{siteTitle}</h1>
+          <h5>{siteSubtitle}</h5>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+      <Toggle />
+    </div>
+  </header>
+);
 
-export default connect(
-  ({ darkTheme }) => ({ darkTheme }),
-  dispatch => ({ toggleDarkTheme: value => dispatch({ type: 'DARK_THEME', data: value }) }),
-)(Header);
+export default Header;
