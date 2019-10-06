1fb2a7e4ac29e941705a6a578625724decb6ff37
Stefanos Anagnostou <anagstef@gmail.com>
2019-10-07 02:08:13 +0300

    Add "Copied" notification when clicking a css var

--- src/components/list-item.jsx
+++ src/components/list-item.jsx
@@ -1,26 +1,50 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import './list-item.scss';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const ListItem = ({ content }) => (
-  <div className="list-item">
-    <div className="list-item-title">
-      <h2>{content.title}</h2>
-      <a href={content.url} target="_blank" rel="noopener noreferrer"><span>&nbsp;</span></a>
-    </div>
-    <div className="table">
-      {content.cssVars.map(cssVarWithDesc => (
-        <div className="table-row" key={cssVarWithDesc.cssVar}>
-          <div className="row-name">
-            <CopyToClipboard text={cssVarWithDesc.cssVar}>
-              <span>{cssVarWithDesc.cssVar}</span>
-            </CopyToClipboard>
-          </div>
-          <div className="row-desc">{cssVarWithDesc.cssDesc}</div>
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { copiedValue: '' };
+    this.addCopiedText = this.addCopiedText.bind(this);
+  }
+
+  addCopiedText(valueClicked) {
+    this.setState({ copiedValue: valueClicked });
+    setTimeout(() => {
+      this.setState({ copiedValue: '' });
+    }, 2000);
+  }
+
+  render() {
+    const { content } = this.props;
+    const { copiedValue } = this.state;
+    return (
+      <div className="list-item">
+        <div className="list-item-title">
+          <h2>{content.title}</h2>
+          <a href={content.url} target="_blank" rel="noopener noreferrer"><span>&nbsp;</span></a>
         </div>
-      ))}
-    </div>
-  </div>
-);
+        <div className="table">
+          {content.cssVars.map(cssVarWithDesc => (
+            <div className="table-row" key={cssVarWithDesc.cssVar}>
+              <div className="row-name">
+                <CopyToClipboard text={cssVarWithDesc.cssVar}>
+                  <span onClick={() => this.addCopiedText(cssVarWithDesc.cssVar)}>
+                    {cssVarWithDesc.cssVar}
+                  </span>
+                </CopyToClipboard>
+                <div className={`copied-text ${copiedValue === cssVarWithDesc.cssVar ? 'copied-text-clicked' : ''}`}>Copied!</div>
+              </div>
+              <div className="row-desc">{cssVarWithDesc.cssDesc}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ListItem;
